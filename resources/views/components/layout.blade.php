ceafb3b3e55c4e2662d2f0dd0b4094c602c79300
Tangimark <mark@teamtangible.com>
2024-09-17 09:15:04 -0400

    add href fix to nav bar.

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -20,19 +20,19 @@
     <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-user">
       <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
         <li>
-          <a href="/" class="{{ request()->is('/') ? 'block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500' : 'block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700' }}" {{ request()->is('/') ?'aria-current="page"' : ''}}>Home</a>
+          <x-nav-link href="/" :active="request()->is('/')">Home</x-nav-link>
         </li>
         <li>
-          <a href="/rules" class="{{ request()->is('rules') ? 'block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500' : 'block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700' }}" {{ request()->is('rules') ?'aria-current="page"' : ''}}>Rules</a>
+          <x-nav-link href="/rules" :active="request()->is('rules')">Rules</x-nav-link>
         </li>
         <li>
-          <a href="/replays" class="{{ request()->is('replays') ? 'block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500' : 'block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700' }}" {{ request()->is('replays') ?'aria-current="page"' : ''}}>Replays</a>
+          <x-nav-link href="/replays" :active="request()->is('replays')">Replays</x-nav-link>
         </li>
         <li>
-          <a href="/seasons" class="{{ request()->is('seasons') ? 'block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500' : 'block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700' }}" {{ request()->is('seasons') ?'aria-current="page"' : ''}}>Seasons</a>
+          <x-nav-link href="/seasons" :active="request()->is('seasons')">Seasons</x-nav-link>
         </li>
         <li>
-          <a href="/streams" class="{{ request()->is('streams') ? 'block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500' : 'block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700' }}" {{ request()->is('streams') ?'aria-current="page"' : ''}}>Streams</a>
+          <x-nav-link href="/streams" :active="request()->is('streams')">Streams</x-nav-link>
         </li>
       </ul>
     </div>
