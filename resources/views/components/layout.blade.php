6979410d45544907a22530167ad5bece4c8b969d
Danny Moon <dmoon1112@gmail.com>
2024-10-17 13:43:09 -0400

    New Database and installed breeze

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -11,8 +11,8 @@
   <title>{{ $title }}</title>
 </head>
 <body>
-  <div class="bg-white">
-    <header class="absolute inset-x-0 top-0 z-50">
+  <div>
+    <header class="inset-x-0 top-0 z-50">
       <nav class="bg-navbar flex items-center justify-between p-6 lg:px-8" aria-label="Global">
         <div class="flex lg:flex-1">
           <x-nav-link href="/" :active="request()->is('/')" class="-m-1.5 p-1.5">
@@ -66,6 +66,9 @@
         </div>
       </div>
     </header>
+    <div class="container mx-auto">
+      {{ $slot }}
+    </div>
   </div>
 </body>
 </html>
--- resources/views/forum.blade.php
+++ resources/views/forum.blade.php
@@ -2,5 +2,4 @@
   <x-slot:title>
     Forum
   </x-slot:title>
-  <h1>Forum</h1>
 </x-layout>
