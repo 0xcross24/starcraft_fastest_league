6e80e622c36c0dc803f27c70cc90864eb0fbb4ca
Tangimark <mark@teamtangible.com>
2025-09-13 12:02:14 -0400

    update user seeder to give more realistic data

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -9,37 +9,41 @@ use Illuminate\Database\Seeder;
 
 class UserSeeder extends Seeder
 {
+    
+    protected $users = [
+        ['player_name' => '[SFL]Cross-',      'email' => 'Cross@email.com'],
+        ['player_name' => '[SFL]Bigwan',      'email' => 'Bigwan@example.com'],
+        ['player_name' => '[SFL]Dizzy',       'email' => 'Dizzy@example.com'],
+        ['player_name' => '[SFL]gbomb',       'email' => 'gbomb@example.com'],
+        ['player_name' => '[SFL]nightstand',  'email' => 'nightstand@example.com'],
+        ['player_name' => 'Bidulgi',          'email' => 'Bidulgi@example.com'],
+        ['player_name' => 'BL1ZZ',            'email' => 'BL1ZZ@example.com'],
+        ['player_name' => '[SFL]Shrink',      'email' => 'Shrink@example.com'],
+        ['player_name' => 'Vince88',          'email' => 'Vince88@example.com'],
+        ['player_name' => 'flowcc',           'email' => 'flowcc@example.com'],
+        ['player_name' => '[SFL]tb`Hero-',    'email' => 'tb1s@example.com'],
+        ['player_name' => '[SFL]Banging',     'email' => 'Banging@example.com'],
+        ['player_name' => '[SFL]Health',      'email' => 'Health@example.com'],
+        ['player_name' => '[SFL]fil0',        'email' => 'fil0@example.com'],
+        // Add more specific users as needed
+    ];
+
     /**
      * Run the database seeds.
      */
     public function run()
     {
         // Create specific users with custom data
-    
-        User::create([
-            'player_name' => 'tb1s',
-            'email' => 'jane@example.com',
-            'password' => Hash::make('password456'),
-        ]);
-    
-        User::create([
-            'player_name' => 'bigwan3000',
-            'email' => 'uniq1@example.com',
-            'password' => Hash::make('password789'),
-        ]);
-
-        User::create([
-            'player_name' => 'BidulgiAvenger',
-            'email' => 'uniq2@example.com',
-            'password' => Hash::make('password789'),
-        ]);
-
-        User::create([
-            'player_name' => 'AnthZ',
-            'email' => 'uniq3@example.com',
-            'password' => Hash::make('password789'),
-        ]);
-    
-        // Add more specific users as needed
+        foreach ( $this->users as $user ) {
+            User::create([
+              'player_name'       => $user['player_name'],
+              'email'             => $user['email'],
+              'email_verified_at' => now(),
+              'password'          => Hash::make('password456'),
+              'remember_token'    => \Illuminate\Support\Str::random(10),
+              'created_at'        => now(),
+              'updated_at'        => now(),
+            ]);
+        }     
     }
 }
